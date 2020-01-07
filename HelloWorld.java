8ab447780e5bd20ff6e4f64b374607d6aeb9a614
Rob Hess <robwhess@gmail.com>
2020-01-07 15:22:42 -0800

    Add code from 1/7 lecture.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
 class HelloWorld {
   public static void main(String[] args) {
+    boolean b;
+    char c;
+    int i;
+    short s;
+    byte by;
+    long l;
+    float f;
+    double d;
+
+    String str;
+
     System.out.println("Hello, world!");
   }
 }
