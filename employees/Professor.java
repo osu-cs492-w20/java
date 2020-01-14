2d18931d8d5ee3ec466015d942b27ab198a4f0b5
Rob Hess <robwhess@gmail.com>
2020-01-14 13:55:35 -0800

    Add Professor constructor.

--- employees/Professor.java
+++ employees/Professor.java
@@ -1,6 +1,10 @@
 package employees;
 
 public class Professor extends Instructor {
+  public Professor(String name, int id, double salary, int appointmentMonths) {
+    super(name, id, salary, appointmentMonths);
+  }
+
   public String deliverLecture() {
     return "Much pontification...";
   }
